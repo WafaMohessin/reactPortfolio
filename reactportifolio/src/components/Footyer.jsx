1895aa49c3e2eced4e6969776a08ca54543f0372
Wafa Mohessin <wafamohessin@gmail.com>
2022-02-04 22:26:38 -0500

    adding About contact footer and header .jsx

---
+++ reactportifolio/src/components/Footyer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+    return (
+        <div className="footer">
+            <footer className="py-3 bg-dark fixed-bottom">
+                <div className="container">
+                    <p className="m-0 text-center text-white">
+                        copyright &copy; WafaMohessin 2022
+                    </p>
+
+                    </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
