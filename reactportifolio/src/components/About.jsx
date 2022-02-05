4f631245b5f6760b877c6797b56c00189fc41681
Wafa Mohessin <wafamohessin@gmail.com>
2022-02-04 22:20:33 -0500

    create components folder

---
+++ reactportifolio/src/components/About.jsx
@@ -0,0 +1,25 @@
+function About() {
+    return (
+        <div className="about">
+            <div className="container">
+                <div className="row align-items-center my-7">
+                    <div className="col-lg-5">
+                        <h1 className="font-weight-light">About</h1>
+                        <p>
+                        Here in my portfolio page, I show some of my work as a full stack developer. I have succeeded in bootcamp from Michigan State University. I am so excited to put what I learn in practice.
+                        </p>
+                    
+                        <p>
+                        My ultimate goal is to help solve the issues icreasing in health sectore and world of charity by educating stuff and employess and improve people lives. I hope that I will be the link piece between people and world awarnces.
+                            
+                        </p>
+                        </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
+
+
