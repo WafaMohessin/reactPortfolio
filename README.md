54587a2ea1f0a058f78e005fe798bb481141a3e6
Wafa Mohessin <wafamohessin@gmail.com>
2022-02-06 01:43:48 -0500

    readme.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # reactPortfolio
+
+I am using my react skill to buil my portfolio. 
+
+# Technology used 
+- React
+- Javascript
+
+# Screen Shot
+![Wafa Mohessin](./image/Wafa.png)
--- reactportifolio/src/components/Sidebar.jsx
+++ reactportifolio/src/components/Sidebar.jsx
@@ -17,14 +17,7 @@ function Sidebar() {
                 <a href="https://www.linkedin.com/in/wafa-mohessin-7145a047/"> <FontAwesomeIcon icon={["fab", "linkedin"]} /></a>
                 <a href="https://github.com/WafaMohessin"><FontAwesomeIcon icon={["fab", "github"]} /></a>
             </div>
-            {/* <nav className="sidebar-nav">
-                <ul>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#portfolio">Portfolio</a></li>
-                    <li><a href="assets/docs/resume.pdf" target="_blank">Resume</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                </ul>
-            </nav>  */}    
+           
         </div>
         
     )
