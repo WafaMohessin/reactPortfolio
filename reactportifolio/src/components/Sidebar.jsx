4070a97892e3ba362be97f01c3c46d70e7d9011d
Wafa Mohessin <wafamohessin@gmail.com>
2022-02-05 23:09:30 -0500

    broken code in sidebar.jsx

--- reactportifolio/src/components/Sidebar.jsx
+++ reactportifolio/src/components/Sidebar.jsx
@@ -5,24 +5,24 @@ function Sidebar() {
         <div className="side-bar">
             <img src="https://avatars.githubusercontent.com/u/87388655?v=4" alt="Wafa Avatar Picture" />
             <div className="sidebar-text">
-                <p>Wafa Mohessin</p>
-                <p>Front-End/ Back-End Web Application Developer</p>
+                <p className="mainName">Wafa Mohessin</p>
+                <p>Front-end/Back-end</p>
+                <p>Web Application Developer</p>
             </div>
             <div className="contact-icons">
-                <p>GET IN TOUCH</p>
+                <p>Get in Touch</p>
                 <a href="mailto:wafamohessin@gmail.com"> <FontAwesomeIcon icon="envelope" /></a>
                 <a href="https:https://www.linkedin.com/in/wafa-mohessin-7145a047/"> <FontAwesomeIcon icon={["fab", "linkedin"]} /></a>
                 <a href="https:https://github.com/WafaMohessin"><FontAwesomeIcon icon={["fab", "github"]} /></a>
             </div>
-           
-            <nav className="sidebar-nav">
+            {/* <nav className="sidebar-nav">
                 <ul>
                     <li><a href="#about">About</a></li>
                     <li><a href="#portfolio">Portfolio</a></li>
                     <li><a href="assets/docs/resume.pdf" target="_blank">Resume</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
-            </nav>     
+            </nav>  */}    
         </div>
     )
 }
